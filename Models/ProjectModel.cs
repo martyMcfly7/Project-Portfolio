013fbb230181f33131411aa1d1da29c7c9f29709
Victor E Ortiz <42853057+martyMcfly7@users.noreply.github.com>
2023-01-28 13:36:57 -0800

    Changed model, fixed nullable date, use Begin.Form() and minor formatting

--- Models/ProjectModel.cs
+++ Models/ProjectModel.cs
@@ -18,24 +18,21 @@ namespace Projects.Models
         public bool PublicProject { get; set; }
         
         [Display(Name = "Languages Used: ")]
-        [Required(ErrorMessage = "You must list a language!")]
         public string Language { get; set; }
-        
+
+        [DataType(DataType.MultilineText)]
         [Display(Name = "About the Project: ")]
-        [Required(ErrorMessage = "You must have an about!")]
         public string About { get; set; }
-        
+
+        [DataType(DataType.MultilineText)]
         [Display(Name = "Project Description: ")]
-        [Required(ErrorMessage = "You must have a description!")]
         public string Description { get; set; }
         
         [DataType(DataType.Date)]
         [Display(Name = "Created Date: ")]
-        [Required(ErrorMessage = "You must have a created date!")]
         public DateTime? CreatedDate { get; set; }
 
         [Display(Name = "Image Name: ")]
-        [Required(ErrorMessage = "You must have an image filename!")]
         public string ImageName { get; set; }
     }
 }
